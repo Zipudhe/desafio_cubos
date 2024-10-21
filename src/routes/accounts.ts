58558c79a52a5e7d0d68f6aab966506fba9240e7
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-10-21 12:43:53 -0300

    feature: transaction and cards routes

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -15,18 +15,47 @@ type CardData = {
   cvv: string,
 }
 
+type TransactionsData = {
+  value: number,
+  description: string
+}
+
+type InternalTransaction = TransactionsData & {
+  receiverAccountId: string
+}
+
 router.post('/', (req: Request<{}, {}, AccountData>, res: Response) => {
   CreatedHandler({}, res)
 })
 
-router.post('/:accountId/cards', (req: Request<{}, {}, CardData>, res: Response) => {
+router.post('/:accountId/cards', (req: Request<{ accountId: string }, {}, CardData>, res: Response) => {
   CreatedHandler({}, res)
 })
 
-router.get('/', (req: Request<{}, {}, AccountData>, res: Response) => {
+router.post('/:accountId/transactions', (req: Request<{ accountId: string }, {}, TransactionsData>, res: Response) => {
+  CreatedHandler({}, res)
+})
+
+router.post('/:accountId/transactions/internal', (req: Request<{ accountId: string }, {}, InternalTransaction>, res: Response) => {
+  CreatedHandler({}, res)
+})
+
+router.post('/:accountId/transactions/:transactionId/revert', (req: Request<{ accountId: string, transactionId: string }, {}, {}>, res: Response) => {
+  CreatedHandler({}, res)
+})
+
+router.get('/', (req: Request<{}, {}, {}>, res: Response) => {
   SuccesHandler({}, res)
 })
 
-router.get('/:accountId/cards', (req: Request<{}, {}, CardData>, res: Response) => {
+router.get('/:accountId/cards', (req: Request<{}, {}, {}>, res: Response) => {
+  SuccesHandler({}, res)
+})
+
+router.get('/:accountId/transactions', (req: Request<{ accountId: string }, {}, {}>, res: Response) => {
+  CreatedHandler({}, res)
+})
+
+router.get('/:accountId/balance', (req: Request<{ accountId: string }, {}, {}>, res: Response) => {
   CreatedHandler({}, res)
 })
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import Express from 'express'
 import { router as PeopleRouter } from './people'
 import { router as AccountsRouter } from './accounts'
+import { router as CardsRouter } from './accounts'
 
 export const router = Express.Router()
 
 router
   .use("/people", PeopleRouter)
   .use("/accounts", AccountsRouter)
+  .use("/cards", CardsRouter)
 
