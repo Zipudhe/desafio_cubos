059e2d4a6f69401f98cfcb1efd2321faaa370a4c
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-10-21 11:27:11 -0300

    feature: route error handlers

---
+++ src/handlers/ErrorHandler.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express'
+
+type ErrorHandler = (message: string, res: Response) => void
+
+export const NotFound: ErrorHandler = (message, res) => {
+  const statusMessage = message ?? 'unable to find resource'
+  res.status(404).json({ message: statusMessage })
+}
+
+export const UnAuthorized: ErrorHandler = (message, res) => {
+  const statusMessage = message ?? 'user must be authenticated'
+  res.status(401).json({ message: statusMessage })
+}
+
+export const UnprocessableContent: ErrorHandler = (message, res) => {
+  const statusMessage = message ?? 'Unable to process action'
+  res.status(422).json({ message: statusMessage })
+}
+
+export const BadRequest: ErrorHandler = (message, res) => {
+  const statusMessage = message ?? 'some parameters are missing'
+  res.status(400).json({ message: statusMessage })
+}
