0df6a00f233bd7e0f921b30b35ab1fe510395b86
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-10-21 12:26:50 -0300

    feature: cards router

---
+++ src/routes/cards.ts
@@ -0,0 +1,24 @@
+import Express, { Request, Response } from 'express'
+
+import { SuccesHandler } from '../handlers/SuccessHandler'
+
+export const router = Express.Router()
+
+type PaginationData = {
+  itemsPerPage: number,
+  currentPage: number
+}
+
+
+type CardData = {
+  id: string,
+  type: string,
+  number: string,
+  cvv: string,
+  createdAt: Date,
+  updatedAt: Date
+}
+
+router.get('/', (req: Request<{}, {}, CardData, PaginationData>, res: Response) => {
+  SuccesHandler({}, res)
+})
