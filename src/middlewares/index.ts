88762fc06b703dde4ba7b89a898bce9244aa61fd
lucas.ramalho <ramalhodev.lucas@gmail.com>
2024-10-25 08:26:32 -0300

    feature: auth middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+import { UnAuthorized } from "../handlers/ErrorHandler";
+
+dotenv.config()
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const { headers } = req
+
+  if (!headers['authorization']) {
+    return UnAuthorized('missing token', res)
+  }
+
+  const secret = process.env.JWT_SECRET
+  const token = headers['authorization'].replace('Bearer ', '')
+
+  try {
+    const jwtData = jwt.verify(token, secret) as JwtPayload
+
+    req.body = { ...req.body, personId: jwtData.data.id }
+
+    next()
+  } catch (error) {
+    return UnAuthorized(error.message, res)
+  }
+}
